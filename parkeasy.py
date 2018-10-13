057c5c0cfb9ccfcecfa69b6fd4c19d2379949a4e
Aditya Sreepad <aditya.g.sreepad@gmail.com>
2018-10-14 02:39:59 +0530

    updated
    done

--- parkeasy.py
+++ parkeasy.py
@@ -1,5 +1,5 @@
 from flask import Flask , jsonify , request , make_response
-#from flask_restful import Resource, Api
+from flask_restful import Resource, Api
 #from flask_mysqldb import MySQL
 #from functools import wraps
 
