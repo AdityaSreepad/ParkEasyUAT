0504d6489219d3c607fb87be19bf22d41af1a439
Aditya Sreepad <aditya.g.sreepad@gmail.com>
2018-10-14 19:57:42 +0530

    Modified - simple welcome page

--- parkeasy.py
+++ parkeasy.py
@@ -1,16 +1,10 @@
-from flask import Flask , jsonify , request , make_response
-from flask_restful import Resource, Api
-#from flask_mysqldb import MySQL
-#from functools import wraps
-
-import jwt
-import datetime
-
+from flask import Flask
 app = Flask(__name__)
 
 #home page clause
 @app.route('/')
 def index():
     return 'welcome to the app'
+
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
